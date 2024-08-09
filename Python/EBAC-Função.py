cc3df6f4b246d2402394fcf752543293457b29d9
NetzZz0 <enzong10@gmail.com>
2024-08-09 08:11:17 -0300

    Meu primeira HTML

---
+++ Python/EBAC-Função.py
@@ -0,0 +1,16 @@
+# Função-Triangulo retangulo
+'''def somaquadrado(a, b):
+    quadrado = (a*a) + (b*b)
+    return quadrado
+hip = int(input('Digite a hipotenusa do triangulo: '))
+cat1 = int(input('Digite o segundo lado: '))
+cat2 = int(input('Digite o terceiro lado: '))
+if (hip > cat1) and (hip > cat2):
+    print('Você digitou valores adequados!')
+    calculo =somaquadrado(cat1,cat2)
+    if (hip*hip)== calculo:
+        print('Esse tringulo é um triangulo retangulo')
+    else:
+        print('Esse tringulo não é um triangulo retangulo')
+else:
+    print('Esses valores não são compativeis, tente novamente')'''
