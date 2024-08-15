d361c46748a8262cccae9996d0d706fd4b18b0ce
NetzZz0 <enzong10@gmail.com>
2024-08-15 16:13:38 -0300

    Validando ou invalidando nomeCompleto

--- Front-end/JavaScript/Modulo_7/main.js
+++ Front-end/JavaScript/Modulo_7/main.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+const form = document.getElementById('form-deposito')
+
+function validName(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
+}
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const nomeBeneficiario = document.getElementById('nome-beneficiario')
+    if (validName(nomeBeneficiario.value) == false) {
+        alert('Nome invalido');
+    } else {
+        alert('Nome valido');
+    }
+})
+
+console.log(form)
