a9c607f77f813870779ca66b4660724a26106a5d
NetzZz0 <enzong10@gmail.com>
2024-08-15 17:02:31 -0300

    add and style error-msg

--- Front-end/JavaScript/Modulo_7/main.js
+++ Front-end/JavaScript/Modulo_7/main.js
@@ -1,4 +1,6 @@
 const form = document.getElementById('form-deposito')
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEValido = false
 
 function validName(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
@@ -6,25 +8,40 @@ function validName(nomeCompleto) {
 }
 
 form.addEventListener('submit', function (e) {
-    let formEValido = false
+
     e.preventDefault();
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
     const msgDeSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>`;
 
     formEValido = validName(nomeBeneficiario.value)
     if (formEValido) {
-        document.querySelector('.success-msg').innerHTML = msgDeSucesso;
+        const containerMsgDeSucesso = document.querySelector('.success-msg');
+        containerMsgDeSucesso.innerHTML = msgDeSucesso;
+        containerMsgDeSucesso.style.display = 'block';
 
         nomeBeneficiario.value = '';
         numeroContaBeneficiario.value = '';
         valorDeposito.value = '';
 
     } else {
-        alert('Nome invalido');
+        nomeBeneficiario.style.border = '1px solid red'
+        document.querySelector('.error-msg').style.display = 'block';
     }
 })
 
-console.log(form)
+nomeBeneficiario.addEventListener('keyup', function (e) {
+    console.log(e.target.value)
+    formEValido = validName(e.target.value);
+
+    if (!formEValido) {
+        nomeBeneficiario.classList.add('error');
+        document.querySelector('.error-msg').style.display = 'block';
+    } else {
+        nomeBeneficiario.classList.remove('error');
+        document.querySelector('.error-msg').style.display = 'none';
+    }
+
+});
