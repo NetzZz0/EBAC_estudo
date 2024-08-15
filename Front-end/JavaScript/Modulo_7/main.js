f24f8208b056197c92630b2b76dcfccbafe70925
NetzZz0 <enzong10@gmail.com>
2024-08-15 16:37:15 -0300

    estilizando msgDeSucesso

--- Front-end/JavaScript/Modulo_7/main.js
+++ Front-end/JavaScript/Modulo_7/main.js
@@ -6,13 +6,24 @@ function validName(nomeCompleto) {
 }
 
 form.addEventListener('submit', function (e) {
+    let formEValido = false
     e.preventDefault();
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario')
-    if (validName(nomeBeneficiario.value) == false) {
-        alert('Nome invalido');
+    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const msgDeSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>`;
+
+    formEValido = validName(nomeBeneficiario.value)
+    if (formEValido) {
+        document.querySelector('.success-msg').innerHTML = msgDeSucesso;
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+
     } else {
-        alert('Nome valido');
+        alert('Nome invalido');
     }
 })
 
