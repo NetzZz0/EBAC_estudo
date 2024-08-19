fb2c45561f3f0c1c651e6c871ca5355b505ce95a
NetzZz0 <enzong10@gmail.com>
2024-08-19 14:11:54 -0300

    adicionando as img ao dite atraves do JQuery

--- Front-end/JavaScript/Modulo_9/main.js
+++ Front-end/JavaScript/Modulo_9/main.js
@@ -9,5 +9,16 @@ $(document).ready(function () {
 
     $('form').on('submit', function (e) {
         e.preventDefault();
+        const adressNewImg = $('#adress-new-img').val();
+        const newItem = $('<li style="display: none;"></li>');
+        $(`<img src="${adressNewImg}" />`).appendTo(newItem);
+        $(`
+            <div class="img-link">
+                <a href="${adressNewImg}" target="_black" title="Ver em tamanho real">Ver imagem em tamanho real</a>
+            </div>
+            `).appendTo(newItem);
+        $(newItem).appendTo('ul');
+        $(newItem).fadeIn(1000);
+        $('#adress-new-img').val('')
     })
 }) 
